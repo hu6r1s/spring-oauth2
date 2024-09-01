7f4df38491c9b5267d3d2b72469b35395917cbee
hu6r1s <darmaa333@gmail.com>
2024-09-01 16:20:51 +0900

    init commit

---
+++ src/main/java/me/hu6r1s/oauth2/Oauth2Application.java
@@ -0,0 +1,13 @@
+package me.hu6r1s.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Oauth2Application.class, args);
+  }
+
+}
---
+++ src/test/java/me/hu6r1s/oauth2/Oauth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hu6r1s.oauth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
