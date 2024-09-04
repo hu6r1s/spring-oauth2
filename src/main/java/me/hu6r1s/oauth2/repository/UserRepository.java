c614cb7d5fd49732ccfcaee81621f68cf14ff1c3
hu6r1s <darmaa333@gmail.com>
2024-09-04 21:50:48 +0900

    feat: JWT Authentication Filter

--- src/main/java/me/hu6r1s/oauth2/repository/UserRepository.java
+++ src/main/java/me/hu6r1s/oauth2/repository/UserRepository.java
@@ -4,5 +4,5 @@ import me.hu6r1s.oauth2.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+  User findByUserId(String userId);
 }
