b9043f37766e3803af37f7c110f539d35b5aff95
hu6r1s <darmaa333@gmail.com>
2024-09-07 19:18:01 +0900

    feat: Certification Mail Func

---
+++ src/main/java/me/hu6r1s/oauth2/global/provider/MailProvider.java
@@ -0,0 +1,42 @@
+package me.hu6r1s.oauth2.global.provider;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MailProvider {
+
+  private final JavaMailSender javaMailSender;
+  private final String SUBJECT = "[TEST 메일] 인증메일";
+
+  public boolean sendCertificationMail(String mail, String certificationNumber) {
+    try {
+      MimeMessage message = javaMailSender.createMimeMessage();
+      MimeMessageHelper messageHelper = new MimeMessageHelper(message, true);
+
+      String htmlContent = getCertificationMessage(certificationNumber);
+
+      messageHelper.setTo(mail);
+      messageHelper.setSubject(SUBJECT);
+      messageHelper.setText(htmlContent, true);
+
+      javaMailSender.send(message);
+    } catch (MessagingException e) {
+      return false;
+    }
+
+    return true;
+  }
+
+  private String getCertificationMessage(String certificationNumber) {
+    String certificationMessage = "";
+    certificationMessage += "<h1 style='text-align: center;'>[TEST 메일] 인증메일 테스트 제목</h1>";
+    certificationMessage += "<h3 style='text-align: center'>인증코두 : <strong style='font-size: 32px; letter-spacing: 8px;'>" + certificationNumber + "</strong></h3>";
+    return certificationMessage;
+  }
+}
