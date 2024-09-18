543e24a0740e479c9415a148abe2d5fe6d12b4f0
hu6r1s <darmaa333@gmail.com>
2024-09-18 15:57:47 +0900

    feat: API Specification

--- src/main/java/me/hu6r1s/oauth2/Oauth2Application.java
+++ src/main/java/me/hu6r1s/oauth2/Oauth2Application.java
@@ -1,9 +1,12 @@
 package me.hu6r1s.oauth2;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "OAuth2.0 및 JWT 로그인 / 회원가입 API 명세서", description = "OAuth2.0, JWT 로그인 / 회원가입 학습하기 위한 API 명세서", version = "v1"))
 public class Oauth2Application {
 
   public static void main(String[] args) {
--- src/main/java/me/hu6r1s/oauth2/controller/AuthController.java
+++ src/main/java/me/hu6r1s/oauth2/controller/AuthController.java
@@ -1,5 +1,11 @@
 package me.hu6r1s.oauth2.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import me.hu6r1s.oauth2.dto.request.CheckCertificationRequestDto;
@@ -19,10 +25,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
+@Tag(name = "JWT 로그인 / 회원가입 API 명세서", description = "JWT를 이용한 로그인 및 회원가입 API 명세입니다.")
 public class AuthController {
 
   private final AuthService authService;
 
+  @Operation(summary = "유저 아이디 중복 체크", description = "유저 아이디가 중복되었는지 확인하는 API")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Success", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CertificationResponseDto.class))),
+      @ApiResponse(responseCode = "400", description = "Validation failed", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CertificationResponseDto.class))),
+      @ApiResponse(responseCode = "400", description = "Duplication Id", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CertificationResponseDto.class))),
+      @ApiResponse(responseCode = "500", description = "Database error", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CertificationResponseDto.class))),
+  })
   @PostMapping("/id-check")
   public ResponseEntity<CertificationResponseDto> idCheck(
       @RequestBody @Valid IdCheckRequestDto requestDto
@@ -31,6 +45,14 @@ public class AuthController {
     return CertificationResponseDto.success();
   }
 
+  @Operation(summary = "이메일 인증", description = "이메일로 인증 코드를 보내는 API")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Success", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CertificationResponseDto.class))),
+      @ApiResponse(responseCode = "400", description = "Validation failed", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CertificationResponseDto.class))),
+      @ApiResponse(responseCode = "400", description = "Duplication Id", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CertificationResponseDto.class))),
+      @ApiResponse(responseCode = "500", description = "Mail Send failed", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CertificationResponseDto.class))),
+      @ApiResponse(responseCode = "500", description = "Database error", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CertificationResponseDto.class))),
+  })
   @PostMapping("/email-certification")
   public ResponseEntity<CertificationResponseDto> mailCertification(
       @RequestBody @Valid MailCertificationRequestDto requestDto
@@ -39,6 +61,13 @@ public class AuthController {
     return CertificationResponseDto.success();
   }
 
+  @Operation(summary = "이메일 인증 코드 체크", description = "이메일로 온 인증 코드를 확인하는 API")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Success", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CertificationResponseDto.class))),
+      @ApiResponse(responseCode = "400", description = "Validation failed", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CertificationResponseDto.class))),
+      @ApiResponse(responseCode = "401", description = "Certification failed", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CertificationResponseDto.class))),
+      @ApiResponse(responseCode = "500", description = "Database error", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CertificationResponseDto.class))),
+  })
   @PostMapping("/check-certification")
   public ResponseEntity<CertificationResponseDto> checkCertification(
       @RequestBody @Valid CheckCertificationRequestDto requestDto
@@ -47,14 +76,29 @@ public class AuthController {
     return CertificationResponseDto.success();
   }
 
+  @Operation(summary = "회원가입", description = "유저 회원가입 API")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Success", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CertificationResponseDto.class))),
+      @ApiResponse(responseCode = "400", description = "Validation failed", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CertificationResponseDto.class))),
+      @ApiResponse(responseCode = "400", description = "Duplication", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CertificationResponseDto.class))),
+      @ApiResponse(responseCode = "401", description = "Certification failed", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CertificationResponseDto.class))),
+      @ApiResponse(responseCode = "500", description = "Database error", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CertificationResponseDto.class))),
+  })
   @PostMapping("/signup")
   public ResponseEntity<CertificationResponseDto> signup(
-    @RequestBody @Valid SignUpRequestDto requestDto
+      @RequestBody @Valid SignUpRequestDto requestDto
   ) {
     authService.signup(requestDto);
     return CertificationResponseDto.success();
   }
 
+  @Operation(summary = "로그인", description = "유저 로그인 API")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Success", content = @Content(mediaType = "application/json", schema = @Schema(implementation = SignInResponseDto.class))),
+      @ApiResponse(responseCode = "400", description = "Validation failed", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CertificationResponseDto.class))),
+      @ApiResponse(responseCode = "401", description = "Login information mismatch", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CertificationResponseDto.class))),
+      @ApiResponse(responseCode = "500", description = "Database error", content = @Content(mediaType = "application/json", schema = @Schema(implementation = CertificationResponseDto.class))),
+  })
   @PostMapping("/signin")
   public ResponseEntity<SignInResponseDto> signin(
       @RequestBody @Valid SignInRequestDto requestDto
